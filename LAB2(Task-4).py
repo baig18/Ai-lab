aceeee01eadb0ec31ea45058ba85966edbabcbd5
baig18 <43401222+baig18@users.noreply.github.com>
2018-09-19 01:41:39 -0700

    Lab1-Task4

---
+++ LAB2(Task-4).py
@@ -0,0 +1,79 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Noman Baig")
+
+
+# In[4]:
+
+for i in range(1,101):
+ print(i)
+
+
+# In[13]:
+
+year =int(input("Enter Year:")) 
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("This is a leap year")
+       else:
+           print("This is not a leap year")
+   else:
+       print("This is a leap year")
+else:
+   print("This is not a leap year")
+
+
+    
+
+
+# In[15]:
+
+s="Noman Baig"
+s=[1:3]
+
+
+# In[19]:
+
+x="Hello World"
+x=[-1::-1]
+print(x)
+
+
+# In[17]:
+
+x="Hello world"
+
+
+# In[18]:
+
+x=[-1::-1]
+
+
+# In[21]:
+
+Name= "Noman Baig"
+x=Name[-1::-1]
+print(x)
+
+
+# In[23]:
+
+s = input("Input a string")
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+print("Letters", l)
+print("Digits", d)
+
+
+# In[ ]:
+
+
+
